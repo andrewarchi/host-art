bea6fe2a17c970feac90bd9c06e02ced1953c2ef
Andrew Archibald <andrew@aarchibald.com>
2019-12-01 14:59:07 -0700

    Clean up print script

--- print.sh
+++ print.sh
@@ -1,11 +1,12 @@
-# Print ASCII art on login to specific machines
-AA_LOGIN_ART=~/.login_art/$(hostname)
-[ ! -f "$AA_LOGIN_ART" ] && [ ! -f "${AA_LOGIN_ART}_color" ] &&
-  AA_LOGIN_ART=~/.login_art/byu
-echo
-if [ -f "${AA_LOGIN_ART}_color" ]; then
-  echo -e "$(cat ${AA_LOGIN_ART}_color)" | head -n -1
+#!/bin/bash
+
+# Print ASCII art corresponding to current machine
+art_file=$(hostname)
+[ ! -f "$art_file" ] && [ ! -f "${art_file}_color" ] &&
+  art_file=byu
+
+if [ -f "${art_file}_color" ]; then
+  echo -e "$(cat ${art_file}_color)" | head -n -1
 else
-  head -n -1 "$AA_LOGIN_ART"
+  head -n -1 "$art_file"
 fi
-echo
