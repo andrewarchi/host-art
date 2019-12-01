93f1071264622e89ee9a7068f05715c4d6c52cd2
Andrew Archibald <andrew@aarchibald.com>
2019-12-01 15:20:40 -0700

    Print relative to script directory

--- print.sh
+++ print.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Print ASCII art corresponding to current machine
-art_file=$(hostname)
+dir=$(dirname "$0")
+art_file="$dir/$(hostname)"
 [ ! -f "$art_file" ] && [ ! -f "${art_file}_color" ] &&
-  art_file=byu
+  art_file="$dir/byu"
 
 if [ -f "${art_file}_color" ]; then
   echo -e "$(cat ${art_file}_color)" | head -n -1
